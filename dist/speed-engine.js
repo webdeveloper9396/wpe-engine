9a460e4b4615dfd5184d3f3201845316cdc4635b
webdeveloper9396 <rahulkumarwebdeveloper9396@gmail.com>
2026-06-30 18:41:28 +0530

    Refactor speed-engine.js to define WPE object
    new code updated

--- dist/speed-engine.js
+++ dist/speed-engine.js
@@ -1,3 +1,17 @@
-// TEMP BUILD FILE
 console.log("WPE Engine Loaded");
-window.WPE = window.WPE || {};
+
+window.WPE = {
+  version: "1.0.0",
+
+  init: function () {
+    console.log("WPE Initialized");
+  },
+
+  speedCheck: function () {
+    console.log("Checking website performance...");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  WPE.init();
+});
